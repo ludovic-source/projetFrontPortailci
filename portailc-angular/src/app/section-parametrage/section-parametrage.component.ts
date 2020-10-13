51ad27433601db30268be9273d41ce3d5cad08e1
ludovic <ludovic.ch.lc@gmail.com>
2020-10-13 19:02:46 +0200

    Amélioration du module de paramétrage.

--- portailc-angular/src/app/section-parametrage/section-parametrage.component.ts
+++ portailc-angular/src/app/section-parametrage/section-parametrage.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionParametrageComponent implements OnInit {
 
-  typeParametrage = '';
+  typeParametrage = 'utilisateur';
 
   constructor() {}
 
